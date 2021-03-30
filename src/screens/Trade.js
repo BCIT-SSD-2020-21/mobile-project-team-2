9820a511fb7599521f60f848b7e402d8d0acefca
Thelma Palma <design@thelmapalma.com>
2021-03-30 16:11:39 -0700

    added function calculator

--- src/screens/Trade.js
+++ src/screens/Trade.js
@@ -16,7 +16,19 @@ const [lastNumber, setLastNumber] = useState('')
 
 const buttons = ['C', 'DEL', '/', 7,8,9, '*', 4,5,6, '-', 1,2,3, '+', 0, '.', '=']
 
-
+function calculator() {
+    
+    let lastArr = currentNumber[currentNumber.length-1];
+    
+    if(lastArr === '/' || lastArr === '*' || lastArr === '-' || lastArr === '+' || lastArr === '.') {
+      setCurrentNumber(currentNumber)
+      return
+    }
+    else {
+      let result = eval(currentNumber).toString();
+      setCurrentNumber(result)
+      return
+    }
 
 
     return (
