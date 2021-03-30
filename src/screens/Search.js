46268cc354d3a7109e5d914ff19b8ef72d67cd4d
Chris Bayne <chriss.bayne@gmail.com>
2021-03-30 16:11:43 -0700

    Added search api - working

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -3,6 +3,9 @@ import { StyleSheet, View, Text, TextInput, Keyboard, ScrollView, TouchableOpaci
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
 import stockapi from '../api/stockapi'
 import { Ionicons } from '@expo/vector-icons'
+// import { useStockContext } from '../context/stockContext'
+import { API_KEY, BASE_URL } from 'dotenv'
+import axios from 'axios';
 
 function PromptText ({ children }) {
 	return (
@@ -41,52 +44,80 @@ function StockList({ stocks, addStockToWatchList }) {
 	)
 }
 
-export default function Search() {
-	const { baseURL, addToWatchList } = useStocksContext()
-	const [state, setState] = useState({
-		stocks: [], // all stocks
-		searchText: "",
-		filteredStocks: [], // stocks filtered by the searchText
-	})
+export default function Search({ navigation }) {
+	// const { baseURL, addToWatchList } = useStockContext()
 
-	useEffect(() => {
-		getStocks()
-	},[])
+	const [searchText, setSearchText] = useState("")
+	const [filteredStocks, setFilteredStocks] = useState([])
+
+	// useEffect(() => {
+	// 	getOneStock("GME")
+	// },[])
+
+	// useEffect(() => {
+	// 	fetch(`${baseURL}/all`)
+	// 	.then(res => res.json())
+	// 	.then(stockData => {setState(prev => ({ ...prev, stocks: stockData })) })
+	// 	.catch(err => console.error(err))
+	// }, [])
 
 	const handleChangeSearchText = (text) => {
 		text = text.replace(/[.*+\-?^${}()|[\]\\]/g, ''); //prevent the error caused by entering special characters
 		const regex = RegExp(text, "i")
-		setState(prev => ({
-			...prev,
-			searchText: text,
-			filteredStocks: state.stocks.filter(stock => regex.test(stock.symbol) || regex.test(stock.name))
-		}))
+		// setState(prev => ({
+		// 	...prev,
+		// 	// filteredStocks: state.stocks.filter(stock => regex.test(stock.symbol) || regex.test(stock.name))
+		// }))
+		setSearchText(text)
 	}
 
-	const getStocks = async () => {
+	useEffect(() => {
+		if (searchText.length > 1) {
+			searchStocks(searchText)
+		}
+	}, [searchText])
+
+
+	const getOneStock = async ({ symbol }) => {
 		try {
-			const response = await stockapi.get(`/quote?symbol=GME`)
+			const response = await stockapi.get(`/quote?symbol=${symbol}`)
 			console.log(response)
 		} catch (err) {
 			console.error('API Call error:', err)
 		}
 	}
 
+	const searchStocks = async ( text ) => {
+		try {
+			console.log("searchTerm", text, stockapi)
+			const response = await axios.get(`${BASE_URL}/search?q=${text}&token=${API_KEY}`)// await stockapi.get(`/search?q=${text}&token=${API_KEY}`)
+			console.log(response.data.result)
+			setFilteredStocks(response.data.result)
+		} catch (err) {
+			console.error('API Call error:', err)
+		} 
+	}
+
+	const handleAddStockToWatchList = (stock) => {
+		addToWatchList(stock.symbol)
+		navigation.navigate('Stocks')
+	}
+
     return (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <TouchableOpacity onPress={Keyboard.dismiss}>
 			<View style={styles.container}>
 				<PromptText>Type a company name or stock symbol</PromptText>
 
 				<SearchBox 
-				searchText={state.searchText}
+				searchText={searchText}
 				handleChangeSearchText={handleChangeSearchText}
 				/>
 				
-				{state.searchText !== "" && 
-				<StockList stocks={state.filteredStocks} addStockToWatchList={handleAddStockToWatchList}
+				{searchText !== "" && 
+				<StockList stocks={filteredStocks} addStockToWatchList={handleAddStockToWatchList}
 				/>}
 			</View>
-		</TouchableWithoutFeedback>
+		</TouchableOpacity>
     )
 }
 
@@ -94,11 +125,13 @@ const styles = StyleSheet.create({
 	container: {
         flex: 1,
     },
+
 	promptText: {
 		marginTop: 10,
 		marginBottom: 10,
 		textAlign: "center"
 	},
+
 	searchSection: {
 		flexDirection: 'row',
 		alignItems: 'center',
@@ -108,13 +141,39 @@ const styles = StyleSheet.create({
 		marginHorizontal: 3,
 		marginTop: 5,
 	},
+
 	searchIcon: {
 		paddingHorizontal: 15,
 		fontSize: 20,
 	},
+
 	searchInput: {
 		flex: 1, // get the rest of the space
-	  },
+	},
+
+	stockListItem: {
+		paddingBottom: 10,
+		borderBottomColor: "red",
+		borderBottomWidth: 1
+	},
+
+	stockSymbol: {
+		paddingHorizontal: 10,
+		paddingTop: 10,
+		color: "blue",
+		fontSize: 20
+	},
+
+	stockName: {
+		paddingHorizontal: 10,
+		color: "black"
+	},
+
+	divider: {
+		marginTop: 10,
+		borderBottomColor: "grey",
+		borderBottomWidth: 1
+	}
 });
 
 
