c1f7889b98caacc146e1d0ac142cc234d297a6ef
Thelma Palma <design@thelmapalma.com>
2021-03-30 16:02:25 -0700

    added const

--- src/screens/Trade.js
+++ src/screens/Trade.js
@@ -10,6 +10,15 @@ const styles = StyleSheet.create({
 });
 
 export default function Trade() {
+
+const [currentNumber, setCurrentNumber] = useState('');
+const [lastNumber, setLastNumber] = useState('')
+
+const buttons = ['C', 'DEL', '/', 7,8,9, '*', 4,5,6, '-', 1,2,3, '+', 0, '.', '=']
+
+
+
+
     return (
         <SafeAreaView style={styles.container}>
             <Text>Trade Screen</Text>
