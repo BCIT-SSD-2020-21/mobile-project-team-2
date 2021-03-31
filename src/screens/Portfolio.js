4f4846169620605776467d8b8a4d7440bf5e0095
veeepi <vpreduna90@gmail.com>
2021-03-31 01:13:31 -0700

    Portfolio Screen styling

--- src/screens/Portfolio.js
+++ src/screens/Portfolio.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import * as firebase from 'firebase'
 import {
 	StyleSheet,
 	SafeAreaView,
@@ -10,46 +9,59 @@ import {
 	Image,
 	View
 } from 'react-native';
+import { firebase } from '../../firebase/config';
+import { EvilIcons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
 	container: {
         flex: 1,
+		display: 'flex',
 		alignItems: "center",
 		justifyContent: "center",
-		padding: 100,
+		// padding: 100,
 		fontFamily: 'Roboto',
+		backgroundColor: '#0f1d12', //dark-green
     },
-	image : {
+	icon: {
+		margin: 'auto',
+		fontSize: 328,
+		color: "#59a66b",
+	},	
+	image: {
 		width:400, 
 		height:400, 
-	},	
-
+	},
 	button: { 
 		display: 'block',
-		width: 300,
-		backgroundColor: '#147DF0',
+		width: '32%',
+		margin: '0',
+		// backgroundColor: '#147DF0',
+		backgroundColor: '#59a66b', // medium-green
 	}
 });
-
-
 
 export default function Portfolio({navigation}) {
 	const [error, setError] = useState('')
 	const onSignout = () => {
-		
 		firebase.auth().signOut()
-			.then((result) => {setError("sucess")})
-			.catch((error) => {setError(error) })
-	 }	 
-
+			.then(() => setError("User SignedOut"))
+			.catch((error) => setError(error.message))
+	 }
     return (
         <SafeAreaView style={styles.container}>
 		<View>
+			<Button 
+				style={styles.button} 
+				onPress={onSignout}
+				color="#59a66b" 
+				title="Sign Out" />
+			<Text style={styles.greeting}>{}</Text>
+			<EvilIcons style={styles.icon} name="chart" size={30} color="black" />
 			{/* an image placeholder for TODO task */}
-			<Image style={styles.image}  />
+			<Image style={styles.image} />
 			<View>
 				<Text>{error}</Text>
-				<Button style={styles.button} onPress={onSignout} 	title="Sign Out" />
+				
 			</View>		
 		</View>
         </SafeAreaView>
