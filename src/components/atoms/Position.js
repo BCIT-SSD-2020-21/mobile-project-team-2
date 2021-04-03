3359e5ce4c2c275b9488dc0e94afb57d3e5181c0
veeepi <vpreduna90@gmail.com>
2021-04-03 01:20:45 -0700

    +Position comp; fetch pos from FB, info from FH

---
+++ src/components/atoms/Position.js
@@ -0,0 +1,104 @@
+import React, {useState, useEffect} from 'react'
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+// import { useEffect } from 'react/cjs/react.development';
+import { getStockProfile, getStockQuote } from '../../api/stockapi';
+import {firebase} from '../../firebase/config';
+
+export default function Position({onPress, positionId}) {
+
+    // console.log("Position: positionId PROP: ", positionId)
+
+    const [loaded, setLoaded] = useState(false)
+    
+    // const [stockSymbol, setStockSymbol] = useState(symbol)
+    const [position, setPosition] = useState({})
+    const [stockDescription, setStockDescription] = useState("")
+    const [currentPrice, setCurrentPrice] = useState("")
+
+    	// GET THE USER OBJECT (contains cashOnHand, Watchlist, OwnedStocksList)
+	function fetchPosition() {
+        if (positionId) {
+            // (firestoreDB) ref=collection, get user obj via onSnapshot, where id=positionId
+            const ref = firebase.firestore().collection('positions').doc(positionId)
+            ref.onSnapshot((doc) => {
+                console.log("current data: ", doc.data());
+                setPosition(doc.data());
+            });
+        }
+	}
+	useEffect(() => {
+		fetchPosition();
+	}, [])
+	console.log("position: ", position);
+
+    useEffect(() => {
+      if (position?.symbol) {
+        (async () => {
+          const profileResult = await getStockProfile(position?.symbol);
+          console.log('profileResult: ', profileResult)
+          setStockDescription(profileResult.name)
+        })();
+      }
+    }, [position])
+
+    useEffect(() => {
+      if (position?.symbol) {
+        (async () => {
+          const quoteResult = await getStockQuote(position?.symbol);
+          console.log('quoteResult: ', quoteResult)
+          setCurrentPrice(quoteResult.c)
+        })();
+      }
+    }, [position])
+
+    console.log("position: ", position);
+    return (
+        // REVIEW API Response Data
+    <TouchableOpacity style={styles.itemContainer}>
+        <View style={styles.itemInfoSection}>
+            <Text style={styles.itemSymbol}>{position?.symbol}</Text>
+            <Text style={styles.itemDescription}>{stockDescription}</Text>
+        </View>
+        {/* <View> */}
+            {/* { currentPrice &&  */}
+              <Text style={styles.currentPrice}>{currentPrice}</Text>
+            {/* } */}
+        {/* </View> */}
+    </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    // ...
+    itemContainer: {
+      // marginTop: 5,
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    itemInfoSection: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    itemSymbol: {
+      fontSize: 32,
+      fontWeight: "bold",
+    //   alignSelf: "flex-start",
+      textTransform: "uppercase"
+    },
+    itemDescription: {
+        fontSize: 24,
+        color: "#000",
+        // alignSelf: "flex-start",
+    },
+    currentPrice: {
+        fontSize: 36,
+        color: "#000",
+        fontWeight: "bold",
+        // alignSelf: "flex-end",
+        textTransform: "uppercase"
+      },
+      priceDetail: {
+        fontSize: 18,
+      }
+  });
