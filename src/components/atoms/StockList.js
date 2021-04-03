a20a1df74171bec461b2ee95f55051a98bf6d15c
veeepi <vpreduna90@gmail.com>
2021-04-02 22:32:58 -0700

    +StockList component (atom); list StockListItem(s)

---
+++ src/components/atoms/StockList.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import { View } from 'react-native'; // StyleSheet, SafeAreaView, ScrollView, TouchableOpacity, Text, TextInput, 
+// import { firebase } from '../firebase/config';
+// import { EvilIcons } from '@expo/vector-icons';
+// import { getStockProfile, getStockQuote } from '../../api/stockapi';
+// import { API_KEY, BASE_URL } from 'dotenv'
+// import axios from 'axios';
+import StockListItem from './StockListItem';
+
+export default function StockList({ stockArray }) {
+    const [stockList, setStockList] = useState([])
+    useEffect(() => {
+
+    }, [])
+    
+    console.log("stockArray", stockArray)
+    return (
+        <View> 
+            {
+                stockArray?.map((prop, index) => {
+                    return (
+                        <StockListItem key={index} symbol={prop} />
+                    )
+                })
+            }
+
+            {/* {
+                stockArray?.filter(stockSymbol => {
+                    if (stockSymbol) {
+                        return (
+                            <StockListItem symbol={stockSymbol} />
+                        )
+                    }
+                })
+            } */}
+        </View>
+    )
+}
