3b1893d4347db60cbd48f0e66d0068a619ae3637
jianmingtu <tu_jm@yahoo.com>
2021-04-04 23:25:30 -0700

    #33 - 1) added watch button 2) added Stats( including Current, High, Low price, currency,  profile2, 3) disable the Sell button if user does not have this stock in position.

--- src/screens/StockDetail.js
+++ src/screens/StockDetail.js
@@ -8,7 +8,8 @@ import {
 	Text,
 	Image,
 	View,
-	ScrollView
+	ScrollView,
+	Linking
 } from 'react-native';
 import { API_KEY, BASE_URL } from 'dotenv'
 import axios from 'axios';
@@ -17,6 +18,7 @@ import { EvilIcons } from '@expo/vector-icons';
 import { firebase } from '../firebase/config';
 import { getStockQuantity } from '../firebase/service';
 import { getStockProfile, getStockQuote } from '../api/stockapi';
+
 
 
 const styles = StyleSheet.create({
@@ -38,7 +40,15 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		backgroundColor:  '#147DF0',
+		position: 'relative',	
 	},	
+	watch : {
+		position: 'absolute',
+		top: 0,
+		right: 0,
+		width: 50,
+		height: 50,
+	},
 	companyDefaultLogo : {
 		width: 60,
 		height: 60,
@@ -73,23 +83,29 @@ const styles = StyleSheet.create({
 		alignItems: 'flex-start',
 	},
 	activities : {
-		fontSize: 36,
+		fontSize: 26,
 		marginVertical: 10,
 	},
-	activity: {
-		display: 'flex',
+    itemInfoSection: {
+
 		width: '100%',
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		marginVertical: 5,
-	},	
-	activityLeft : {
-		width: '60%',
-		fontSize: 20,
-	},
-	activityRight : {
-		fontSize: 20,
-	},
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+		alignItems: 'center',
+		borderBottomWidth: 1,
+		borderBottomColor: '#555555',
+    },
+    itemSymbol: {
+      fontSize: 28,
+      fontWeight: "bold",
+    //   alignSelf: "flex-start",
+      textTransform: "uppercase"
+    },
+    itemDescription: {
+        fontSize: 24,
+        color: "#000",
+        // alignSelf: "flex-start",
+    },  
 	company: {
 		display: 'flex',
 		width: '100%',
@@ -99,7 +115,7 @@ const styles = StyleSheet.create({
 		marginVertical: 5,
 	},	
 	companyInfo : {
-		fontSize: 36,
+		fontSize: 30,
 		marginVertical: 10,
 	},
 	companyLogo: {
@@ -113,14 +129,14 @@ const styles = StyleSheet.create({
 		width: "100%",
 		flexDirection: 'row',
 		justifyContent: 'space-between',
-		marginVertical: 5,
+		marginVertical: 10,
 	},	
     buttonLeft: {
 		backgroundColor:  '#147DF0',
 		alignItems: 'center',
 		justifyContent: 'center',
 		width: '50%',
-		padding: 5,
+		padding: 4,
 		marginLeft: 0,
 		marginHorizontal: 10,
     },	
@@ -129,15 +145,15 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 		width: '50%',
-		padding: 5,
+		padding: 4,
 		marginRight: 0,
 		marginHorizontal: 10,		
     },	
     buttonText: {
 
 		color: 'white',
-		fontSize: 24, 
-		height: 40, 
+		fontSize: 20, 
+		height: 30, 
     },		
   card : {
     flex: 1,
@@ -146,12 +162,12 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   cardHeader : {
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: 'bold'
 
   },
   cardDetail : {
-    fontSize: 20,
+    fontSize: 16,
     marginLeft: 10
 
   },
@@ -166,51 +182,48 @@ const StockDetail = ({ route, navigation}) => {
 	const [currentPrice, setCurrentPrice] = useState(null)
 	const [stockProfile, setStockProfile] = useState({})	
 	const [stockQuote, setStockQuote] = useState({})
+	const [position, setPosition] = useState(false)
 
-	// const getProfile = async ({ symbolVal }) => {
-	// 	try {
-	// 		const response = await axios.get(`${BASE_URL}/stock/profile?symbol=${symbolVal}&token=${API_KEY}`)
-	// 		setStock(response.data)
-	// 	} catch (err) {
-	// 		console.error('API Call error:', err)
-	// 	}
-	// }
+    useEffect( () => {
+		(async() => {
+			if (symbol) {
+				const profileResult = await getStockProfile(symbol);
+				console.log('StockDetail, profileResult: ', profileResult)
+				setStockProfile(profileResult)
 
-	// const getCurrentPrice = async () => {
-	// 	try {
-	// 		const response = await axios.get(`${BASE_URL}/quote?symbol=${symbolVal}&token=${API_KEY}`)
-	// 		setCurrentPrice(response.data.c)
-	// 	} catch (err) {
-	// 		console.error('API Call error:', err)
-	// 	}
-	// }	
-
-	// useEffect( async () => {
-	// 	await getProfile({symbolVal})
-	// 	await getCurrentPrice({symbolVal})
-
-	// 	var {email} = firebase.auth().currentUser
-	// 	const quantity = await getStockQuantity({email, symbol: symbolVal})
-	// 	console.log('quantity', quantity);
-	// },[])
-    useEffect(() => {
-		if (symbol) {
-		  (async () => {
-			const profileResult = await getStockProfile(symbol);
-			console.log('StockDetail, profileResult: ', profileResult)
-			setStockProfile(profileResult)
-		  })();
-		}
+				const quoteResult = await getStockQuote(symbol);
+				console.log('StockDetail, quoteResult : ', quoteResult)
+				setStockQuote(quoteResult)			
+		}})()
 	  }, [symbol])
-	  useEffect(() => {
-		if (symbol) {
-		  (async () => {
-			const quoteResult = await getStockQuote(symbol);
-			console.log('StockDetail, quoteResult: ', quoteResult)
-			setStockQuote(quoteResult)
-		  })();
-		}
-	  }, [symbol])
+
+	useEffect(() => {
+		// find if the current user has the stock
+
+		const userUID = firebase.auth().currentUser.uid
+		// get user document by user UID; setUser		
+		const userDoc = firebase.firestore().collection('users')
+						.doc(userUID)
+						.onSnapshot((doc) => {
+							const {positions} = doc?.data()
+							positions?.map( positionId => {  
+								const positions = firebase.firestore().collection('positions')
+									.doc(positionId)
+									.onSnapshot((doc) => {
+										if (doc?.data().symbol === symbol && doc?.data().quantity > 0) 
+										{ 
+											setPosition(true) 
+											console.log("position", doc.data())
+										}							
+									})
+								
+							})
+						})
+						
+					
+	
+		
+	},[])	  
 
 	  function toTrade() {
 		  if (symbol) {
@@ -236,10 +249,21 @@ const StockDetail = ({ route, navigation}) => {
 			}
 		}	
 
+		function toAddWatch() {
+			const userUID = firebase.auth().currentUser.uid
+			console.log('toAddWatch userUID:', userUID)
+			const userRef = firebase.firestore().collection('users').doc(userUID)
+			userRef.update({
+				watchList: firebase.firestore.FieldValue.arrayUnion(symbol)
+			});
+
+		}			
+
 	// console.log("StocKDetial, route: ", route.params)
 	console.log("StocKDetial, symbol: ", symbol)
 	console.log("StocKDetial, stockProfile: ", stockProfile)
 	console.log("StocKDetial, stockQuote: ", stockQuote)
+	console.log("user position: ", position)	
 	
     return (
 		<ScrollView contentContainerStyle ={styles.scrollContainer}>
@@ -248,30 +272,93 @@ const StockDetail = ({ route, navigation}) => {
 			<SafeAreaView style={styles.safeAreaContainer}>
 				<View style={styles.container}>
 					<View style={styles.titleContainer}>
-						<View style={styles.companyLogo}>
+						<TouchableOpacity style={styles.watch} 	onPress={() => toAddWatch()}>
+							<EvilIcons  name='eye' size={50} color='white' />
+						</TouchableOpacity>
+						<View style={styles.companyDefaultLogo}>
 							<Text style={styles.companyLogoName}>{symbol}</Text>
 						</View>
 						<Text style={styles.companyName}>{stockProfile.name}</Text>
-						<Text style={styles.portfolio}>{`$${Math.round(stockQuote.c).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}</Text>
+						<Text style={styles.portfolio}>{stockQuote?.c ? `$${parseFloat(stockQuote.c).toFixed(2)}`:""} {stockProfile?.currency} </Text>
 						<EvilIcons name='chart' size={300} color='white' />
 					</View>
 					<View style={styles.bodyContainer}>
-						<View><Text style={styles.activities}>Activities</Text></View>
-						<View style={styles.activity}> 
-							<Text style={styles.activityLeft}>{'Buy '}</Text>
-							<Text style={styles.activityRight}>${`${currentPrice?.toFixed(2)}`}</Text>
+						<View><Text style={styles.activities}>Stats</Text></View>	
+
+						<View style={styles.itemInfoSection}>
+							<Text style={styles.itemSymbol}>Open</Text>
+							<Text style={styles.itemDescription}>{stockQuote?.o ? `$${parseFloat(stockQuote.o).toFixed(2)}`:""}</Text>
+						</View>									
+
+						<View style={styles.itemInfoSection}>
+							<Text style={styles.itemSymbol}>High</Text>
+							<Text style={styles.itemDescription}> {stockQuote?.h ? `$${parseFloat(stockQuote.h).toFixed(2)}`:""}</Text>
+						</View>			
+
+						<View style={styles.itemInfoSection}>
+							<Text style={styles.itemSymbol}>Low</Text>
+							<Text style={styles.itemDescription}>{stockQuote?.l ? `$${parseFloat(stockQuote.l).toFixed(2)}`:""}</Text>
+						</View>	
+
+						<View style={styles.itemInfoSection}>
+							<Text style={styles.itemSymbol}>Exchange</Text>
+							<Text style={{...styles.itemDescription, fontSize: 20, marginLeft: 20, maxWidth: '50%',} }> {stockProfile.exchange}</Text>
+						</View>							
+
+
+						 <View style={styles.company}> 				
+							<Text style={styles.companyInfo} numberOfLines={1} >Company Info</Text>
+							<Image style={styles.companyLogo} source={{ uri: stockProfile.logo}} />
 						</View>
-						<View style={styles.activity}> 
-							<Text style={styles.activityLeft}>{'Sell '}</Text>
-							<Text style={styles.activityRight}>${`${currentPrice?.toFixed(2)}`}</Text>
-						</View>		
-
 					
-						
-  
-			
-	
 
+						<View style={styles.card}>
+							<Text style={styles.cardHeader}>Exchange:</Text>
+							<Text style={styles.cardDetail}>{stockProfile.exchange}</Text>
+						</View>	
+
+						<View style={styles.card}>
+							<Text style={styles.cardHeader}>Industry:</Text>
+							<Text style={styles.cardDetail}>{stockProfile.finnhubIndustry}</Text>
+						</View>	
+
+						<View style={styles.card}>
+							<Text style={styles.cardHeader}>Country:</Text>
+							<Text style={styles.cardDetail}>{stockProfile.country}</Text>
+						</View>	
+																	
+
+						<View style={styles.card}>
+							<Text style={styles.cardHeader}>IPO:</Text>
+							<Text style={styles.cardDetail}>{stockProfile.ipo}</Text>
+						</View>
+
+						<View style={styles.card}>
+							<Text style={styles.cardHeader}>Telephone:</Text>
+							<Text style={styles.cardDetail}>{stockProfile.phone}</Text>
+						</View>
+
+						<View style={styles.card}>
+							<Text style={styles.cardHeader}>IPO:</Text>
+							<Text style={styles.cardDetail}>{stockProfile.ipo}</Text>
+						</View>							
+
+						<View style={styles.card}>
+							<Text style={styles.cardHeader}>Market Capitalization:</Text>
+							<Text style={styles.cardDetail}>{stockProfile.marketCapitalization}</Text>
+						</View>							
+
+						<View style={styles.card}>
+							<Text style={styles.cardHeader}>Share Outstanding:</Text>
+							<Text style={styles.cardDetail}>{stockProfile.shareOutstanding}</Text>
+						</View>
+
+						<View style={styles.card}>
+							<Text style={styles.cardHeader}>Website:</Text>
+							 <Text style={{color: 'blue'}}	onPress={() => {  Linking.openURL(stockProfile.weburl);  }}>
+							Click here to open {stockProfile.name} Website
+							</Text>
+						</View>								
 
 
 					
@@ -279,10 +366,11 @@ const StockDetail = ({ route, navigation}) => {
 							<TouchableOpacity style={styles.buttonLeft}	onPress={() => toTradeBuyStock()}>
 								<Text style={styles.buttonText}	>{'Buy'}</Text> 
 							</TouchableOpacity>
-							<TouchableOpacity style={styles.buttonRight} onPress={() => toTradeSaleStock()} >
+							<TouchableOpacity onPress={() => position && toTradeSaleStock()} disabled={position} 
+							style={{...styles.buttonLeft,  backgroundColor: position ? '#147DF0':'#555555'}} >
 								<Text style={styles.buttonText}>{'Sell'}</Text>
 							</TouchableOpacity>		
-						</View>			
+						</View>			 
 					</View>
 				</View>
 			</SafeAreaView>
