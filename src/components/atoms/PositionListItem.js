05ea279858c0457fe9ec194d8abd82f189ba4ac8
veeepi <vpreduna90@gmail.com>
2021-04-06 03:44:55 -0700

    PositionListItem styling

--- src/components/atoms/PositionListItem.js
+++ src/components/atoms/PositionListItem.js
@@ -9,6 +9,7 @@ export default function PositionListItem({onPress, position, navigation}) {
     const [stockProfile, setStockProfile] = useState("")
     const [stockQuote, setStockQuote] = useState("")
     const [currentPositionValue, setCurrentPositionValue] = useState(0)
+    const [priceVariance, setPriceVariance] = useState(0)
 
     useEffect(() => {
       if (position?.symbol) {
@@ -27,7 +28,8 @@ export default function PositionListItem({onPress, position, navigation}) {
       }
     }, [position])
     useEffect(() => {
-      setCurrentPositionValue(stockQuote.c*position.quantity)
+      setCurrentPositionValue(stockQuote?.c*position.quantity)
+      setPriceVariance(stockQuote.c - position.averageCostPerShare)
     }, [stockQuote])
 
     function toStockDetail() {
@@ -39,17 +41,61 @@ export default function PositionListItem({onPress, position, navigation}) {
     // console.log("position: ", position);
     return (
     <TouchableOpacity style={styles.container} onPress={() => toStockDetail()}>
+    
+        {/* Left */}
         <View style={styles.profile}>
           <Text style={styles.symbol}>{position?.symbol}</Text>
           <Text style={styles.name}>{stockProfile.name}</Text>
         </View>
-        <View style={styles.amounts}>
-          <Text style={styles.price}>
-            {`$${Math.round(stockQuote.c).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
+
+        {/* Center */}
+        <View style={styles.variances}>
+
+         <View style={styles.positionCenter}> 
+          <Text style={styles.label}>{'price: '}</Text>
+          <Text style={styles.currentPrice}>
+            {stockQuote ? `$${Math.round(stockQuote?.c).toFixed(0).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`: ''}
           </Text>
-          <Text style={styles.positionValue}>
-            {`$${Math.round(currentPositionValue).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
+          <Text style={styles.currentPriceDecimal}>
+            {stockQuote ? `${(stockQuote?.c.toFixed(0)%stockQuote?.c).toFixed(2).toString().substring(1,4)}` : ''}
           </Text>
+          </View>
+
+        <View style={styles.positionCenter}>
+          <Text style={styles.label}>{'var: '}</Text>
+          <Text style={styles.priceVariance}>
+            {`$${Math.round(priceVariance).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
+          </Text>
+          </View>
+        </View>
+
+        {/* total shares, current price, positionValue */}
+        <View style={styles.position}>
+          {/* Center */}
+          
+          <View style={styles.positionRight}>
+            <Text style={styles.label}>{'avg cost: '}</Text>
+            <Text style={styles.averageCost}>
+              {`$${Math.round(position.averageCostPerShare).toFixed(0).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
+            </Text>
+            {/* <Text style={styles.averageCost}>
+              {`$${(position.averageCostPerShare.toFixed(2)).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
+            </Text> */}
+          </View>
+
+          <View style={styles.positionRight}>
+            <Text style={styles.label}>{'own: '}</Text>
+            <Text style={styles.quantityValue}>
+              {`${position.quantity.toFixed(0).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
+            </Text>
+          </View>
+
+          <View style={styles.positionRight}>
+            <Text style={styles.label}>{'value: '}</Text>
+            <Text style={styles.positionValue}>
+              {`$${Math.round(currentPositionValue).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
+            </Text>
+          </View>
         </View>
     </TouchableOpacity>
     )
@@ -60,37 +106,82 @@ const styles = StyleSheet.create({
       display: 'flex',
       flexDirection: 'row',
       justifyContent: 'space-between',
+      borderWidth: 1,
+      margin: 2,
+    },
+    positionLeft: {
+
     },
     profile: {
       flexDirection: 'column',
       justifyContent: 'space-around',
     },
     symbol: {
-      fontSize: 32,
+      textAlign: 'left',
+      fontSize: 24,
       fontWeight: "bold",
       textTransform: "uppercase"
     },
+    
     name: {
-      fontSize: 24,
+      textAlign: 'left',
+      fontSize: 18,
       color: "#000",
     },
     amounts: {
       flexDirection: 'column',
       justifyContent: 'space-around',
     },
-    price: {
-      fontSize: 32,
-      textAlign: 'right',
+    // center
+    positionCenter: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    currentPrice: {
+      fontSize: 20,
+      textAlign: 'center',
       color: "#000",
       textTransform: "uppercase"
+    },
+    currentPriceDecimal: {
+      fontSize: 14,
+      textAlign: 'left',
+      color: "#000",
+      textTransform: "uppercase"
+    },
+    priceVariance: {
+      fontSize: 16,
+      textAlign: 'center',
+      color: "#000",
+      textTransform: "uppercase"
+    },
+    // right
+    position: {
+
+    },
+    positionRight: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    label: {
+      fontSize: 12,
+      textAlign: 'left',
+      fontStyle: 'italic',
+    },
+    quantityValue: {
+      fontSize: 16,
+      textAlign: 'right',
+    },
+    averageCost: {
+      fontSize: 20,
+      textAlign: 'right',
     },
     positionValue: {
-      fontSize: 24,
+      fontSize: 16,
       textAlign: 'right',
       color: "#000",
       textTransform: "uppercase"
     },
-    // priceDetail: {
-    //   fontSize: 18,
-    // }
   });
