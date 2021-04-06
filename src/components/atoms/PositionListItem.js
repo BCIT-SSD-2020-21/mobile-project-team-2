5c63ef981013332f60a12793bad71ae45b30171b
veeepi <vpreduna90@gmail.com>
2021-04-06 02:34:12 -0700

    cleanup PositionListItem component

--- src/components/atoms/PositionListItem.js
+++ src/components/atoms/PositionListItem.js
@@ -5,36 +5,14 @@ import { getStockProfile, getStockQuote } from '../../api/stockapi';
 import {firebase} from '../../firebase/config';
 
 export default function PositionListItem({onPress, position, navigation}) {
-
-    // console.log("Position: positionId PROP: ", positionId)
-
     const [loaded, setLoaded] = useState(false)
-    
-    // const [stockSymbol, setStockSymbol] = useState(symbol)
-    // const [position, setPosition] = useState({})
     const [stockDescription, setStockDescription] = useState("")
     const [currentPrice, setCurrentPrice] = useState("")
 
-    	// GET THE USER OBJECT (contains cashOnHand, Watchlist, OwnedStocksList)
-    // function fetchPosition() {
-    //       if (positionId) {
-    //           // (firestoreDB) ref=collection, get user obj via onSnapshot, where id=positionId
-    //           const ref = firebase.firestore().collection('positions').doc(positionId)
-    //           ref.onSnapshot((doc) => {
-    //               // console.log("current data: ", doc.data());
-    //               setPosition(doc.data());
-    //           });
-    //       }
-    // }
-    // useEffect(() => {
-    //   fetchPosition();
-    // }, [])
-    // console.log("position: ", position);
     useEffect(() => {
       if (position?.symbol) {
         (async () => {
           const profileResult = await getStockProfile(position?.symbol);
-          // console.log('profileResult: ', profileResult)
           setStockDescription(profileResult.name)
         })();
       }
@@ -43,7 +21,6 @@ export default function PositionListItem({onPress, position, navigation}) {
       if (position?.symbol) {
         (async () => {
           const quoteResult = await getStockQuote(position?.symbol);
-          // console.log('quoteResult: ', quoteResult)
           setCurrentPrice(quoteResult.c)
         })();
       }
@@ -57,17 +34,14 @@ export default function PositionListItem({onPress, position, navigation}) {
 
     // console.log("position: ", position);
     return (
-        // REVIEW API Response Data
     <TouchableOpacity style={styles.itemContainer} onPress={() => toStockDetail()}>
         <View style={styles.itemInfoSection}>
-            <Text style={styles.itemSymbol}>{position?.symbol}</Text>
-            <Text style={styles.itemDescription}>{stockDescription}</Text>
+          <Text style={styles.itemSymbol}>{position?.symbol}</Text>
+          <Text style={styles.itemDescription}>{stockDescription}</Text>
         </View>
-        {/* <View> */}
-            {/* { currentPrice &&  */}
-              <Text style={styles.currentPrice}>{`$${Math.round(currentPrice*100/100).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}</Text>
-            {/* } */}
-        {/* </View> */}
+          <Text style={styles.currentPrice}>
+            {`$${Math.round(currentPrice*100/100).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
+          </Text>
     </TouchableOpacity>
     )
 }
