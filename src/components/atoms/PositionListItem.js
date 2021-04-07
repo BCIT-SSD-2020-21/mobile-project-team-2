0297edb0146e9e0c3dfe88c643fdb02d7e2f46a6
veeepi <vpreduna90@gmail.com>
2021-04-07 03:34:33 -0700

    PositionListItem user PriceAmount comp; DRY code

--- src/components/atoms/PositionListItem.js
+++ src/components/atoms/PositionListItem.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { View, TouchableOpacity, Text } from "react-native";
 import { getStockProfile, getStockQuote } from '../../api/stockapi';
 import styles from '../../styles/positionListStyles'
+import PriceAmount from './PriceAmount';
 
 export default function PositionListItem({ position, navigation}) {
     const [stockProfile, setStockProfile] = useState("")
@@ -45,50 +46,24 @@ export default function PositionListItem({ position, navigation}) {
 
 			{/* Center */}
 			<View style={styles.variances}>
-				<View style={styles.positionCenter}> 
-					<Text style={styles.label}>{'price: '}</Text>
-					<Text style={styles.currentPrice}>
-						{stockQuote ? `$${Math.round(stockQuote?.c).toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`: ''}
-					</Text>
-					<Text style={styles.currentPriceDecimal}>
-						{stockQuote ? `${(stockQuote?.c.toFixed(0)%stockQuote?.c).toFixed(2).substring(1,4)}` : ''}
-					</Text>
-				</View>
-
-				<View style={styles.positionCenter}>
-					<Text style={styles.label}>{'var: '}</Text>
-					<Text style={styles.priceVariance}>
-						{`$${Math.round(priceVariance).toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
-					</Text>
-				</View>
+                <PriceAmount label={'cur'} amount={stockQuote? stockQuote.c : 0} />
+                <PriceAmount label={'var'} amount={priceVariance? priceVariance : 0} />
 			</View>
 
 			{/* total shares, current price, positionValue */}
 			<View style={styles.position}>
-				{/* Center */}
-				<View style={styles.positionRight}>
-					 <Text style={styles.label}>{'avg cost: '}</Text>
-					<Text style={styles.averageCost}>
-						{`$${Math.round(position.averageCostPerShare).toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
-					</Text>
-					{/* <Text style={styles.averageCost}>
-						{`$${(position.averageCostPerShare.toFixed(2)).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
-					</Text> */}
-				</View>
 
-				<View style={styles.positionRight}>
-					<Text style={styles.label}>{'own: '}</Text>
+                <PriceAmount label={'cost'} amount={position? position.averageCostPerShare : 0} />
+
+				<View style={styles.quantityBox}>
+					<Text style={styles.quantityLabel}>{'own: '}</Text>
 					<Text style={styles.quantityValue}>
 						{`${parseInt(position?.quantity).toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
 					</Text>
 				</View>
 
-				<View style={styles.positionRight}>
-					<Text style={styles.label}>{'value: '}</Text>
-					<Text style={styles.positionValue}>
-						{`$${Math.round(currentPositionValue).toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`}
-					</Text>
-				</View>
+                <PriceAmount label={'value'} amount={currentPositionValue? currentPositionValue : 0 } />
+
 			</View>
 		</TouchableOpacity>
     )
