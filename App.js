acf0a07f3d5725cb8f83e16662513137973c6cd4
Thelma Palma <design@thelmapalma.com>
2021-04-07 10:25:30 -0700

    Function create and refactor app.js

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AuthStack from './src/components/navigation/AuthStack';
 import BottomTabNavigator from './src/components/navigation/BottomTabNavigator';
-// import DrawerNavigator from './src/components/navigation/DrawerNavigator';
+import DrawerNavigator from './src/components/navigation/DrawerNavigator';
 import { firebase } from './src/firebase/config';
-import UserContext from './src/firebase/UserProvider';
+import UserProvider from './src/context/UserContext';
 import StockDetail from './src/screens/StockDetail';
 import { Platform} from 'react-native';
 
@@ -24,26 +24,16 @@ const PlatformNavigator = Platform.select({
 
 export default function App() {
 
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const subscriber = firebase.auth().onAuthStateChanged((authUser) => {
-      setUser(authUser);	 
-    })
-   return subscriber; // unsubscribe on unmount
-  }, []);
-
-  console.log("authUser", user) 
 
   return (
 
-    <UserContext.Provider value={user}>
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Root" component={user ? PlatformNavigator : AuthNavigator } />
-      </Stack.Navigator>
-    </NavigationContainer>
-    </UserContext.Provider>
+   <UserProvider>
+        <NavigationContainer>
+        	<Stack.Navigator screenOptions={{ headerShown: false }}>
+            	<Stack.Screen name="Root" component={user ? PlatformNavigator : AuthNavigator } />
+      		</Stack.Navigator>
+    	</NavigationContainer>
+    </UserProvider>
  
   );
 }
---
+++ src/context/UserContext.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect, createContext } from 'react';
+import { auth } from "../firebase/config";
+
+const UserContext = createContext()
+
+
+export default function UserProvider(props) {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const subscriber = firebase.auth().onAuthStateChanged((authUser) => {
+            setUser(authUser);	 
+        })
+        return subscriber; // unsubscribe on unmount
+    }, []);
+
+    console.log("authUser", user) 
+
+
+    return( 
+        <UserContext.Provider value={user}>
+            {props.children}
+        </UserContext.Provider>
+    )
+
+
+
+}
+
+
+
+
+/* export default function UserContext ()
+
+const UserContext = createContext();
+
+const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged((authUser) => {
+      setUser(authUser);	 
+    })
+   return subscriber; // unsubscribe on unmount
+  }, []);
+
+  console.log("authUser", user) 
+
+
+
+
+
+export default UserContext;
+ */
+
+
