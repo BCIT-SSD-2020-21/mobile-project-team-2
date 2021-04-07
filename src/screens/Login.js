e674e498217172b9f76828d371ab50eb7c57877a
rachwongrw <rwrachelwong@gmail.com>
2021-04-07 14:54:56 -0700

    #90 - add scroll views

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { SafeAreaView, Text, Image, TextInput, Button, View } from 'react-native';
+import { SafeAreaView,ScrollView, Text, Image, TextInput, Button, View } from 'react-native';
 import {firebase} from '../firebase/config';
 import {resetPassword} from './ResetPassword'
 import styles from '../styles/authStyles'
@@ -17,21 +17,24 @@ export default function Login() {
     }	  
 
     return (
-        <SafeAreaView style={styles.container}>
-            <Image style={styles.image} source={require('../../assets/images/logo.png')} />
-            <View style={{marginBottom: 30}}>
-                <View>
-                    <Text style={styles.label}>Email Address</Text>
-                    <TextInput style={styles.input} autoCorrect={false} autoCapitalize={'none'} onChangeText={(email) => setEmail(email)} />
+        <ScrollView>
+            <SafeAreaView style={styles.container}>
+                <Image style={styles.image} source={require('../../assets/images/logo.png')} />
+                <View style={{marginBottom: 30}}>
+                    <View>
+                        <Text style={styles.label}>Email Address</Text>
+                        <TextInput style={styles.input} autoCorrect={false} autoCapitalize={'none'} onChangeText={(email) => setEmail(email)} />
+                    </View>
+                    <View>
+                        <Text style={styles.label}>Password</Text>
+                        <TextInput style={styles.input} secureTextEntry={true} onChangeText={(password) => setPassword(password)} />
+                    </View>	
+                    <Text style={styles.forgot} onPress={() => resetPassword(email)}>Forgot password or email?</Text>
+                    <Text style={{paddingBottom: 5, color: 'red'}}>{error}</Text>
+                    <Button style={styles.button} onPress={() => onLogin()} title="Login" />
                 </View>
-                <View>
-                    <Text style={styles.label}>Password</Text>
-                    <TextInput style={styles.input} secureTextEntry={true} onChangeText={(password) => setPassword(password)} />
-                </View>	
-                <Text style={styles.forgot} onPress={() => resetPassword(email)}>Forgot password or email?</Text>
-                <Text style={{paddingBottom: 5, color: 'red'}}>{error}</Text>
-                <Button style={styles.button} onPress={() => onLogin()} title="Login" />
-            </View>
-        </SafeAreaView>
+            </SafeAreaView>
+        </ScrollView>
+        
     )
 }
