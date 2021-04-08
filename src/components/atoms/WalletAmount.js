e616a20b8f3f0bb543d1a656407e725a923f11a9
veeepi <vpreduna90@gmail.com>
2021-04-07 20:07:47 -0700

    fix decimal calculations; closes #101

--- src/components/atoms/WalletAmount.js
+++ src/components/atoms/WalletAmount.js
@@ -6,22 +6,31 @@ import { StyleSheet } from 'react-native';
 
 export default function WalletAmount({ label, amount }) {
 
-    const integerAmount = `${Math.round(amount).toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`
-    const decimalAmount = amount ? (amount.toFixed(0)%amount).toFixed(2).substring(1,4) : (0).toFixed(2).substring(1,4)
+    const roundedAmount = amount ? amount.toFixed(2) : 0
+    const integerAmount = amount ? `${amount?.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}` : 0
+    const decimalAmount = amount ? (roundedAmount.substring(roundedAmount.length-3,roundedAmount.length)) : (0).toFixed(2).substring(1,4)
 
     return (
         <View style={walletStyles.wallet}> 
+
+
             <Text style={walletStyles.walletLabel}>{`${label}: `}</Text>
             <View style={walletStyles.walletAmount}>
+    
+    
                 <Text style={walletStyles.walletAmountSymbol}>
                     {'USD$'}
                 </Text>
+
+                {/* <Text style={{ color: '#ffffff'}}>{decimalAmount}</Text> */}
+
                 <Text style={walletStyles.walletAmountInteger}>
                     {integerAmount ? integerAmount : ''}
                 </Text>
                 <Text style={walletStyles.walletAmountDecimal}>
                     {decimalAmount ? decimalAmount : ''}
                 </Text>
+
             </View>
         </View>
     )
